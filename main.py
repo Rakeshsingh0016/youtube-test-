9822e2b9e73a1274d37fe48129e80fd7ee7e0bb3
Rakeshsingh0016 <clickderas@gmail.com>
2026-01-04 09:20:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
-import os
-import requests
+from flask import Flask, request, jsonify
+import os, requests
+
+app = Flask(__name__)
 
 API_KEY = os.environ.get("YOUTUBE_API_KEY")
-VIDEO_ID = "VIDEO_ID_DALO"
-KEYWORD = "nice"
 
-comments = []
-page = ""
+def get_comments(video_id, keyword):
+    comments = []
+    page_token = ""
 
-while True:
-    url = "https://www.googleapis.com/youtube/v3/commentThreads"
-    params = {
-        "part": "snippet",
-        "videoId": VIDEO_ID,
-        "maxResults": 100,
-        "pageToken": page,
-        "key": API_KEY
-    }
+    while True:
+        url = "https://www.googleapis.com/youtube/v3/commentThreads"
+        params = {
+            "part": "snippet",
+            "videoId": video_id,
+            "maxResults": 100,
+            "pageToken": page_token,
+            "key": API_KEY
+        }
 
-    data = requests.get(url, params=params).json()
+        res = requests.get(url, params=params).json()
 
-    for item in data.get("items", []):
-        c = item["snippet"]["topLevelComment"]["snippet"]
-        comments.append({
-            "text": c["textDisplay"],
-            "likes": c["likeCount"]
-        })
+        for item in res.get("items", []):
+            c = item["snippet"]["topLevelComment"]["snippet"]
+            comments.append({
+                "text": c["textDisplay"],
+                "likes": c["likeCount"],
+                "author": c["authorDisplayName"]
+            })
 
-    page = data.get("nextPageToken")
-    if not page:
-        break
+        page_token = res.get("nextPageToken")
+        if not page_token:
+            break
 
-best = None
-for c in comments:
-    if KEYWORD in c["text"].lower():
-        if best is None or c["likes"] > best["likes"]:
-            best = c
+    # Filter keyword + highest like
+    best = None
+    for c in comments:
+        if keyword.lower() in c["text"].lower():
+            if best is None or c["likes"] > best["likes"]:
+                best = c
+    return best
 
-print(best)
+@app.route("/get_comment")
+def get_comment():
+    video_id = request.args.get("video")
+    keyword = request.args.get("keyword", "nice")
+    result = get_comments(video_id, keyword)
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
